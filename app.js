2da881fc0ff0bbe9220d6001a131d7be92bf09ff
Gizelle Emanuela <gizellemanu@hotmail.com>
2023-01-26 14:17:48 -0300

    Teste 03

--- app.js
+++ app.js
@@ -1,9 +1,3 @@
-$(document).ready(function(){
-    $("#submitButton").hover(function() {
-      $(this).css("background-color", "#000000");
-      $(this).css("color", "#ffffff");
-    }, function() {
-      $(this).css("background-color", "#ffffff");
-      $(this).css("color", "#000000");
-    });
-  });
+function myFunction(){
+    document.getElementById('myTopnav').classList.toggle('responsive')
+}
